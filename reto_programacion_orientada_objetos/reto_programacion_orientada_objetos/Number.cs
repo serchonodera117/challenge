5b013625cb5873c51c4a3a0267515ce8cf2c55b8
serchonodera117 <62153337+serchonodera117@users.noreply.github.com>
2022-09-30 00:28:21 -0500

    Add files via upload

---
+++ reto_programacion_orientada_objetos/reto_programacion_orientada_objetos/Number.cs
@@ -0,0 +1,323 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace reto_programacion_orientada_objetos
+{
+    public class Number
+    {
+        private int width;
+        private int height;
+        private int nChar;
+        private string[,] numbers;
+        private string verLine= "|";
+        private string horLine = "_";
+        private string space = " ";
+       public Number(string n)
+        {
+            this.width = 3;
+            this.height = 3;
+            this.nChar = n.Length;
+            numbers = new string[height, nChar];
+        }
+       public Number(int w, int h, string n)
+        {
+            this.width = w;
+            this.height = h;
+            this.nChar = n.Length;
+            numbers = new string[height, nChar];
+        }
+
+        public void SetNumber(string n)
+        {
+            this.nChar = n.Length;
+            for (int i = 0; i < n.Length; i++)
+            {
+                switch (n[i])
+                {
+                    case '0': Zero(i);break;
+                    case '1': One(i);break;
+                    case '2': Two(i);break;
+                    case '3': Three(i);break;
+                    case '4': Four(i);break;
+                    case '5': Five(i);break;
+                    case '6': Six(i);break;
+                    case '7': Seven(i);break;
+                    case '8': Eight(i);break;
+                    case '9': Nine(i); break;
+                    default: Console.Write("Opcion Invalida");break;
+                }
+               
+            }
+        }
+
+        public void Imprimir()
+        {
+            Console.WriteLine($"\nDimensiones: Altura[{height}], Ancho[{width}]\nCaracteres [{nChar}]");
+            string[] a = new string[height];
+           for (int i = 0; i < height; i++)//n lines
+           {
+                for (int j = 0; j < nChar; j++) //col 
+                {
+                        a[i] += numbers[i, j];     //building a single line
+                }
+                a[i] += "\n";
+            }
+
+            for (int i = 0; i < a.Length; i++) Console.Write(a[i]);
+        }
+        private void Zero(int position)
+        {
+            string r1 = ""; 
+            string r2 = ""; 
+            string r3 = ""; 
+            for (int i = 0; i< width; i++) //building horizontal lines
+            {
+                r1 += (i == 0 || i == width - 1) ? space : horLine;//   _    
+                r2+= (i == 0 || i == width - 1) ? verLine : space; //  | |  
+                r3 += (i == 0 || i == width - 1) ? verLine : horLine;//|_| 
+            }
+            for (int i = 0; i< height; i++)//building number with lines
+            {
+                if (i == 0) numbers[i,position] = r1;
+                else if (i == height - 1) numbers[i, position] = r3;
+                else numbers[i, position] = r2;
+            }
+        }
+        private void One(int position)
+        {
+            string r1 = "";
+            string r2 = ""; 
+            for (int i = 0; i < width; i++) //building horizontal lines
+            {
+                r1 += space;
+                r2+= (i == width-1)?verLine: space;// |
+            }
+
+            for (int i = 0; i < height; i++)
+            {
+                if (i == 0) numbers[i,position] = r1;
+                else numbers[i,position] = r2;
+            }
+        }
+        private void Two(int position)
+        {
+            string r1 = "";
+            string r2 = "";
+            string r3 = "";
+            string r4 = "";
+            string r5 = "";
+            for(int i = 0; i < width; i++)
+            {
+                r1 += (i == 0 || i == width - 1) ? space : horLine;        //  _
+                r2 += (i == width - 1) ? verLine :(i!=0)? horLine:space ;  //  _|
+                r4 += (i == 0) ? verLine : space;                          // |
+                r3 += (i == 0) ? verLine : (i != width-1) ? horLine : space;//|_ 
+                r5 += (i == width - 1) ? verLine : space;                   //  |
+
+            }
+            for(int i = 0; i < height; i++)
+            {
+                int half = height/2;
+                if (i == 0) numbers[i, position] = r1;
+                else if (i == height - 1) numbers[i, position] = r3;
+
+                if (height == 3 && i == 1) numbers[i, position] = r2;
+                else if (height > 3)
+                {
+                    if (i == half) numbers[i, position] = r2;
+                    else if (i < half && i > 0) numbers[i, position] = r5;
+                    else if (i > half && i < height - 1) numbers[i, position] = r4;
+                }
+            }
+        }
+        private void Three(int position)
+        {
+            string r1 = "";
+            string r2 = "";
+            string r3 = "";
+            for (int i = 0; i < width; i++) 
+            {
+                r1 += (i == 0 || i == width - 1) ? space : horLine;           // _ 
+                r2 += (i == width - 1) ? verLine : (i != 0) ? horLine : space;// _|
+                r3 += (i == width - 1) ? verLine : space;                     //  |
+
+            }
+            for (int i = 0; i < height; i++)
+            {
+                int half = height / 2;
+                if (i == 0) numbers[i, position] = r1;
+                else if(i == height-1) numbers[i, position] = r2;
+               
+                if (height == 3 && i == half) numbers[i, position] = r2;
+                else if (height > 3)
+                {
+                    if (i == half) numbers[i, position] = r2;
+                    else if (i < half && i > 0) numbers[i, position] = r3;
+                    else if (i > half && i < height-1) numbers[i, position] = r3;
+                }
+            }
+        }
+        private void Four(int position)
+        {
+            string r1 = "";
+            string r2 = "";
+            string r3 = "";
+            string r4 = "";
+            for (int i = 0; i < width; i++)
+            {
+                r1 += space;
+                r2 += (i == 0 || i == width - 1) ? verLine : space;//  | |
+                r3 += (i == 0 || i == width - 1) ? verLine : horLine;//|_|
+                r4 += (i == width - 1) ? verLine : space;            //  |
+            }
+            for (int i = 0; i < height; i++)
+            {
+                int half = height / 2;
+                if (i == 0) numbers[i, position] = r1;
+                else if (i == height - 1) numbers[i, position] = r4;
+
+                if (height == 3 && i == half) numbers[i, position] = r3;
+                else if(height > 3)
+                {
+                    if (i == half) numbers[i, position] = r3;
+                    else if (i > 0 && i < half) numbers[i, position] = r2;
+                    else if (i > half) numbers[i, position] = r4;
+                }
+            }
+        }
+        private void Five(int position)
+        {
+            string r1 = "";
+            string r2 = "";
+            string r3 = "";
+            string r4 = "";
+            string r5 = "";
+            for (int i = 0; i < width; i++) 
+            {
+                r1 += (i == 0 || i == width - 1) ? space : horLine;        //    _
+                r2 += (i == 0) ? verLine : (i==width-1)?space:horLine;     //   |_ 
+                r3 += (i == 0) ? space : (i == width - 1) ? verLine : horLine;// _|
+                r4 += (i == 0) ? verLine : space;                            // |
+                r5 += (i == width - 1) ? verLine : space;                    //   |
+            }
+            for (int i = 0; i < height; i++) 
+            {
+                int half = height / 2;
+                if (i == 0) numbers[i, position] = r1;
+                else if (i == height - 1) numbers[i, position] = r3;
+
+                if (height == 3 && i == half) numbers[i, position] = r2;
+                else if (height > 3)
+                {
+                    if (i == half) numbers[i, position] = r2;
+                    else if (i < half && i > 0) numbers[i, position] = r4;
+                    else if (i > half && i < height - 1) numbers[i, position] = r5;
+                }
+            }
+
+        }
+        private void Six(int position)
+        {
+            string r1 = "";
+            string r2 = "";
+            string r3 = "";
+            string r4 = "";
+            string r5 = "";
+            for (int i = 0; i < width; i++)
+            {
+                r1 += (i == 0 || i == width - 1) ? space : horLine;            // _
+                r2 += (i == 0) ? verLine : (i < width - 1) ? horLine : space; // |_
+                r3 += (i == 0 || i == width - 1) ? verLine : horLine;         // |_|
+                r4 += (i == 0) ? verLine : space; // |  
+                r5 += (i == 0 || i == width - 1) ? verLine : space;//| |
+            }
+            
+            for (int i = 0; i < height; i++)
+            {
+                int half = height / 2;
+                if (i == 0) numbers[i, position] = r1;
+                else if (i == height - 1) numbers[i, position] = r3;
+
+                if (height == 3 && i == half) numbers[i, position] = r2;
+                else if (height > 3)
+                {
+                    if (i == half) numbers[i, position] = r2;
+                    else if (i < half && i > 0) numbers[i, position] = r4;
+                    else if (i > half && i < height - 1) numbers[i, position] = r5;
+                }
+            }
+        }
+        private void Seven(int position)
+        {
+            string r1 = "";
+            string r2 = "";
+            for (int i = 0; i < width; i++)
+            {
+               r1 += (i == 0 || i == width - 1) ? space : horLine; //_
+               r2 += (i == width - 1) ? verLine : space;           // |
+            }
+            for (int i = 0; i < height; i++)
+            {
+                if (i == 0) numbers[i, position] = r1;
+                else numbers[i, position] = r2;
+            }
+        }
+        private void Eight(int position)
+        {
+            string r1 = "";
+            string r2 = "";
+            string r3 = "";
+            for (int i = 0; i < width; i++)
+            {
+                r1 += (i == 0 || i == width - 1) ? space : horLine; //  _
+                r2 += (i == 0 || i == width - 1) ? verLine : space;//  | |
+                r3 += (i == 0 || i == width - 1) ? verLine : horLine;//|_|
+            }
+            for (int i = 0; i < height; i++)
+            {
+                int half = height / 2;
+                if (i == 0) numbers[i, position] = r1;
+                else if (i == height - 1) numbers[i, position] = r3;
+
+                if (height == 3 && i == half) numbers[i, position] = r3;
+                else if(height > 3)
+                {
+                    if (i == half) numbers[i, position] = r3;
+                    else if (i != half && i > 0 && i < height - 1) numbers[i, position] = r2;
+                }
+                
+            }
+        }
+        private void Nine(int position)
+        {
+            string r1 = "";
+            string r2 = "";
+            string r3 = "";
+            string r4 = "";
+            string r5 = "";
+            for (int i = 0; i < width; i++)
+            {
+                r1 += (i == 0 || i == width - 1) ? space : horLine;           // _
+                r2 += (i == 0 || i == width - 1) ? verLine : horLine;        // |_|
+                r3 += (i == width - 1) ? verLine : space;                     //  |
+                r4 += (i == 0) ? space : (i == width - 1) ? verLine : horLine;// _|
+                r5 += (i == 0 || i == width - 1) ? verLine : space;//  | |
+            }
+            for (int i = 0; i < height; i++)
+            {
+                int half = height / 2;
+                if (i == 0) numbers[i, position] =r1;
+                else if(i == height-1) numbers[i, position] =r4;
+
+                if(height == 3 && i == half) numbers[i, position] = r2;
+                else if (height > 3)
+                {
+                    if (i == half) numbers[i, position] = r2;
+                    else if (i > 0 && i < half) numbers[i, position] = r5;
+                    else if (i > half && i < height - 1) numbers[i, position] = r3;
+                }
+            }
+        }
+    }
+}
---
+++ reto_programacion_orientada_objetos/reto_programacion_orientada_objetos/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace reto_programacion_orientada_objetos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char opcion;
+            string inputNumber;
+            int inputHeight, inputWidth;
+            do
+            {
+                Console.Clear();
+                Console.Write("Prueba de programación orientada a objetos\n " +
+                   "opciones: " +
+                   "\n1.-Escribir numero tamaño default (3 ancho y 3 de alto)." +
+                   "\n2.-Escribir número de dimensiones personalizadas." +
+                   "\nS.-Salir" +
+                   "\n\n Escriba el número de la opción elegida : ");
+                opcion = Console.ReadKey().KeyChar;
+
+                switch (opcion)
+                {
+                    case '1':
+                        Console.Clear();
+                        Console.Write("Ha seleccionado escribir un numero de tamaño default (3x2). \n\nEscriba el número: ");
+                        inputNumber = Console.ReadLine();
+                          Console.WriteLine($"Numero [{inputNumber}] ingresado");
+
+                        Number myNumero = new Number(inputNumber);
+                        myNumero.SetNumber(inputNumber);
+                        myNumero.Imprimir();
+                        break;
+                    case '2':
+                        Console.Clear();
+                        try
+                        {
+                            Console.Write("Ha seleccionado Escribir numero de dimensiones personalizadas. \n\nEscriba el tamaño del ancho: ");
+                            inputWidth = int.Parse(Console.ReadLine());
+                            Console.Write("Escriba el tamaño de la altura: ");
+                            inputHeight = int.Parse(Console.ReadLine());
+                            Console.Write("Escriba el número:");
+                            inputNumber = Console.ReadLine();
+
+                            if (inputHeight > 2 && inputWidth > 2)
+                            {
+                                Number numero2 = new Number(inputWidth, inputHeight, inputNumber);
+                                numero2.SetNumber(inputNumber);
+                                numero2.Imprimir();
+                            }
+                            else { Console.Write("No se aceptan dimensiones menores a 3"); }
+                        }catch(Exception e) 
+                        { 
+                            Console.WriteLine($"Error {e.Message}");
+                        }
+                        break;
+                   
+                    case 'S':
+                        opcion = 's';
+                        int efe = 3 / 2;
+                        Console.Write("\nHa seleccionado salir, buen día "+efe);
+                        break;
+                    case 's':
+                        Console.Write("\nHa seleccionado salir, buen día");
+                        break;
+                    default:
+                        Console.WriteLine($"\n\nOpción {opcion} no valida");
+                        break;
+                }
+                Console.ReadKey();
+            } while (opcion != 's');
+        }
+    }
+}
